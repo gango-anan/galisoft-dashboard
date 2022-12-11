575bc4773d05eaa1fcc63917331a6fd3d1760d66
Galiwango Ananiya <galiwango101@gmail.com>
2022-12-12 00:42:16 +0300

    add cart, chart, notification and userProfile menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,6 +37,47 @@ const Navbar = () => {
 				color='blue'
 				icon={<AiOutlineMenu />}
 			/>
+			<div className='flex'>
+				<NavButton
+					title='Cart'
+					customFunc={() => handleClick('cart')}
+					color='blue'
+					icon={<FiShoppingCart />}
+				/>
+				<NavButton
+					title='Chat'
+					dotColor='#03C9D7'
+					customFunc={() => handleClick('chat')}
+					color='blue'
+					icon={<BsChatLeft />}
+				/>
+				<NavButton
+					title='Notification'
+					dotColor='#03C9D7'
+					customFunc={() => handleClick('notification')}
+					color='blue'
+					icon={<RiNotification3Line />}
+				/>
+				<TooltipComponent
+					content='Profile'
+					position='BottomCenter'>
+					<div
+						className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
+						onClick={() => handleClick('userProfile')}>
+						<img
+							className='rounded-full w-8 h-8'
+							src={avatar}
+						/>
+						<p>
+							<span className='text-gray-400 text-14'>Hi, </span>{' '}
+							<span className='text-gray-400 font-bold ml-1 text-14'>
+								Gango
+							</span>
+						</p>
+						<MdKeyboardArrowDown className='text-gray-400 text-14' />
+					</div>
+				</TooltipComponent>
+			</div>
 		</div>
 	);
 };
